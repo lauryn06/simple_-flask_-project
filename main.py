e7259f7824facc255a0bec97839ab34b235785ed
lauryn mwase <laurynmwase06@gmail.com>
2026-04-15 13:08:20 +0200

    i have created the section for pie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 import pandas as pd
+import matplotlib.pyplot as plt
 app=Flask(__name__)
 
 @app.route("/")
@@ -8,9 +9,26 @@ def home():
 
 @app.route("/iris")
 def iris():
-    dataset=pd.read_json("iris.json")
-    return render_template("iris.html", dataset=dataset)
+    return render_template("iris.html")
 
+@app.route("/iris_data")
+def iris_data():
+    dataset=pd.read_json("iris.json")
+    return render_template("iris_data.html", dataset=dataset)
+@app.route("/iris_pie")
+
+def iris_pie():
+   pieChart=pd.read_json("iris.json")
+   counts = pieChart["species"].value_counts()
+   plt.figure()
+   plot_url = plt.pie(counts, labels=counts.index, autopct='%1.1f%%')
+   plt.savefig( format='png')
+   plt.close()
+   return render_template("iris_pie.html", plot_url=plot_url)
+ 
+@app.route("/iris_bar")
+def iris_bar():
+    return render_template("iris_bar.html")
 @app.route("/titanic")
 def titanic():          
     return render_template("titanic.html")
