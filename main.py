81c771a5b232b7839c4c5398f2625f07f66c8149
lauryn mwase <laurynmwase06@gmail.com>
2026-04-15 11:31:58 +0200

    added the iris and titanic routes in main.py file

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app=Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/iris")
+def iris():
+    return render_template("iris.html") 
+
+@app.route("/titanic")
+def titanic():          
+    return render_template("titanic.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
